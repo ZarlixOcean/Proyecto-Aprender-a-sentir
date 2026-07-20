aa53489cd606d5f493beacb667ad379ddb519a12
Samy <jairomorenoprreo@gmail.com>
2026-07-20 17:23:00 -0500

    Arreglar caída al vacío: evitar queue_free en nodo raíz

--- Scripts/zona_de_muerte.gd
+++ Scripts/zona_de_muerte.gd
@@ -2,9 +2,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Personaje"):
-		if body.velocity.y > 0:
+		var parent = get_parent()
+		if parent.get_script() != null and body.velocity.y > 0:
 			body.velocity.y = -380
-			get_parent().queue_free()
+			parent.queue_free()
 		else:
 			GameManager.reset_items()
 			get_tree().reload_current_scene()
