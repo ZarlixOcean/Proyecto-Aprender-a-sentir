0375b7c4efeaee7abb7e52b7547d694ec96e6cd2
Samy <jairomorenoprreo@gmail.com>
2026-07-20 00:39:43 -0500

    Fix: mensajes de alegria ahora se posicionan correctamente en pantalla

--- Scripts/Ui/MensajeAlegria.gd
+++ Scripts/Ui/MensajeAlegria.gd
@@ -1,10 +1,5 @@
 extends CanvasLayer
 
-@onready var start_pos := Vector2(
-	get_viewport().get_visible_rect().size.x + 200,
-	20
-)
-
 func _ready():
 	GameManager.show_alegria_message.connect(_mostrar)
 
@@ -13,17 +8,22 @@ func _mostrar(id: int):
 	if not texture:
 		return
 
+	var screen := get_viewport().get_visible_rect().size
+	var escala := 0.8
+	var tam := texture.get_size() * escala
+
 	var msg := TextureRect.new()
 	msg.texture = texture
 	msg.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-	msg.size = texture.get_size() * 0.8
-	msg.position = start_pos
+	msg.size = tam
 	msg.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	add_child(msg)
 
-	var target_x = get_viewport().get_visible_rect().size.x - msg.size.x - 20
+	var x_final := screen.x - tam.x - 20
+	msg.position = Vector2(screen.x + 20, 20)
+
 	var tween := create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	tween.tween_property(msg, "position:x", target_x, 0.4)
+	tween.tween_property(msg, "position:x", x_final, 0.4)
 	tween.tween_interval(2.5)
 	tween.tween_property(msg, "modulate:a", 0.0, 0.5)
 	tween.tween_callback(msg.queue_free)
