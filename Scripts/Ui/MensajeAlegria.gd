fef262d10a71cb22a0e915e5626a687c5ab1b06c
Samy <jairomorenoprreo@gmail.com>
2026-07-20 01:09:13 -0500

    MensajeAlegria: debug exhaustivo + Label permanente

--- Scripts/Ui/MensajeAlegria.gd
+++ Scripts/Ui/MensajeAlegria.gd
@@ -1,25 +1,29 @@
 extends CanvasLayer
 
-var texturas: Array[Texture2D] = []
-
 func _ready():
+	print("MensajeAlegria: _ready() llamado")
 	follow_viewport_enabled = true
 	layer = 128
-	for i in range(1, 17):
-		var tex = load("res://Sprites/Mensajes/Mensaje_de_alegria_%d.png" % i)
-		if tex:
-			texturas.append(tex)
-		else:
-			print("ERROR: No se pudo cargar textura %d" % i)
-			texturas.append(null)
+
+	var debug := Label.new()
+	debug.text = "DEBUG: MensajesAlegria activo"
+	debug.position = Vector2(10, 10)
+	debug.add_theme_color_override("font_color", Color.GREEN)
+	add_child(debug)
+	print("MensajeAlegria: Label debug agregado")
+
 	GameManager.show_alegria_message.connect(_mostrar)
+	print("MensajeAlegria: Conectado a signal show_alegria_message")
 
 func _mostrar(id: int):
+	print("MensajeAlegria: _mostrar(%d) llamado" % id)
 	if id < 1 or id > 16:
+		print("MensajeAlegria: id fuera de rango")
 		return
-	var texture = texturas[id - 1]
+
+	var texture = load("res://Sprites/Mensajes/Mensaje_de_alegria_%d.png" % id)
+	print("MensajeAlegria: texture cargada = ", texture)
 	if not texture:
-		print("ERROR: Textura %d no disponible" % id)
 		return
 
 	var msg := TextureRect.new()
@@ -30,15 +34,21 @@ func _mostrar(id: int):
 
 	var tam := texture.get_size() * 0.8
 	msg.size = tam
+	print("MensajeAlegria: tam = ", tam)
 
 	var screen := get_viewport().get_visible_rect().size
+	print("MensajeAlegria: screen = ", screen)
 	msg.position = (screen - tam) / 2
+	print("MensajeAlegria: position = ", msg.position)
 
 	add_child(msg)
+	print("MensajeAlegria: TextureRect agregado como hijo")
 
-	var tween := create_tween()
 	msg.modulate.a = 0.0
+	var tween := create_tween()
+	print("MensajeAlegria: tween creado")
 	tween.tween_property(msg, "modulate:a", 1.0, 0.3)
 	tween.tween_interval(2.5)
 	tween.tween_property(msg, "modulate:a", 0.0, 0.5)
 	tween.tween_callback(msg.queue_free)
+	print("MensajeAlegria: animacion configurada")
