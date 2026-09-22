acebb77b7c163be9d6eb099031d74a09352c2be3
Samy <jairomorenoprreo@gmail.com>
2026-09-21 22:29:49 -0500

    cinematicas con musica - uso de ogv + AudioStreamPlayer
    - IntroNivel1.tscn, ExplicacionNivel1.tscn, FinalNivel1.tscn:
      Cambiado de .mp4 a .ogv (carga nativa sin import)
    - ReproducirVideo.gd: Agregado AudioStreamPlayer que reproduce
      Musica nivel alegria.mp3 automáticamente al iniciar video
    - Eliminados .import files de .mp4

--- Scripts/Ui/ReproducirVideo.gd
+++ Scripts/Ui/ReproducirVideo.gd
@@ -2,7 +2,14 @@ extends VideoStreamPlayer
 
 @export var siguiente_escena: String = "res://Escenas/Felicidad_1.tscn"
 
+@onready var musica := AudioStreamPlayer.new()
+
 func _ready():
+	var stream := load("res://Musica y sonidos/Musica nivel alegria.mp3")
+	if stream:
+		musica.stream = stream
+		musica.autoplay = true
+		add_child(musica)
 	finished.connect(_on_video_finished)
 
 func _on_video_finished():
